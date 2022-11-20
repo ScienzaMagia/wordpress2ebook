78c81c4f67151a75ba372194f21596adf8e3df2b
ScienzaMagia <47404591+ScienzaMagia@users.noreply.github.com>
2022-11-20 16:13:13 -0500

    Updated readme

--- README.md
+++ README.md
@@ -1,1 +1,7 @@
 # wordpress2ebook
+
+#   INSTRUCTIONS:
+#   1. Install BeautifulSoup4
+#   2. Create a folder called Rips and a txt document called wpstories.txt in the same directory as this #   3. In wpstories.txt, put one story per line using format "[Story title],[Story URL]"
+#   4. Run "python3 wprip.py"
+    
--- wprip.py
+++ wprip.py
@@ -6,14 +6,6 @@ from bs4 import SoupStrainer
 
 
 class Wprip:
-    
-
-    #   INSTRUCTIONS:
-    #   1. Install BeautifulSoup4
-    #   2. Create a folder called Rips and a txt document called wpstories.txt in the same directory as this script
-    #   3. In wpstories.txt, put one story per line using format "[Story title],[Story URL]"
-    #   4. Run "python3 wprip.py"
-    
 
     def main(self):
 
